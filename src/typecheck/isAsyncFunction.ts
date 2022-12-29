a5e3617dffacca905df874186c252db3305e2e18
wxzhang <wxzhang@126.com>
2022-12-29 10:31:43 +0800

    update isAsyncFunction.ts

--- src/typecheck/isAsyncFunction.ts
+++ src/typecheck/isAsyncFunction.ts
@@ -2,7 +2,8 @@
  *  判断是否是异步函数
  */
 export function isAsyncFunction(fn:any):boolean{
-    return fn.$$isAsync || Object.prototype.toString.call(fn) === '[object AsyncFunction]'
-            || fn.constructor.name === 'AsyncFunction'
+    return (typeof(fn)=="function" && '$$isAsync' in fn)         // 自定义的标识
+        || Object.prototype.toString.call(fn) === '[object AsyncFunction]'
+        || fn.constructor.name === 'AsyncFunction'
 }
 
