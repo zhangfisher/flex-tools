cd09bca923d200d5b8ae96c01fa2b0b64f012e31
wxzhang <wxzhang@126.com>
2023-03-18 14:23:46 +0800

    feat:  re add `packageIsInstalled`

---
+++ src/project/packageIsInstalled.ts
@@ -0,0 +1,32 @@
+import { InvalidProjectPathError } from "../errors"
+import { getProjectRootPath } from "./getProjectRootPath"
+import path from "path"
+import { execShellScript } from "../shell/execShellScript"
+
+/**
+ * 查询指定的包是否安装，如果安装则返回版本号
+ * @param packageName
+ */
+export async function packageIsInstalled(packageName:string,checkGlobal:boolean=false):Promise<boolean>{
+    const packageRoot = getProjectRootPath()
+    if(!packageRoot) throw new InvalidProjectPathError()
+    let installed:boolean = false
+    //
+    try{
+        //require(path.join(packageRoot,`node_modules/${packageName}/package.json`))
+        require.resolve(packageName)
+        installed = true
+    }catch{
+        return false
+    }
+    if(checkGlobal){
+        try{
+            const npmRootPath = await execShellScript("npm root") as  string
+            if(npmRootPath){
+                require(path.join(npmRootPath,`node_modules/${packageName}/package.json`))
+                installed = true
+            }            
+        }catch(e){}        
+    }
+    return installed
+}
