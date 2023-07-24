3ab2edf3368b4b068bdbf35a7e088b1604427140
zhangfisher <wxzhang@126.com>
2023-07-24 16:15:23 +0800

    feat: 新增加`fs`模块实现对node:fs的promiseify

--- src/fs/fs.ts
+++ src/fs/fs.ts
@@ -5,7 +5,7 @@
 import fs from "node:fs"
 import { promisify } from "../func/promisify";
  
-type Overloads<T> =
+type NodejsOverloadsParameters<T> =
   T extends {
     (...args: [...infer A1,any]): infer R1; (...args: [...infer A2,any]): infer R2;
     (...args: [...infer A3,any]): infer R3; (...args: [...infer A4,any]): infer R4
@@ -32,37 +32,35 @@ export * from "node:fs"
 
 type NodejsParameters<T extends (...args: any) => any> = T extends (...args: [...infer P,any]) => any ? P : never;
 
-export const copyFile = promisify<void,NodejsParameters<typeof fs.copyFile>>(fs.copyFile); 
+export const copyFile = promisify<void,NodejsOverloadsParameters<typeof fs.copyFile>>(fs.copyFile); 
 export const exists = promisify<boolean,NodejsParameters<typeof fs.exists>>(fs.exists, {
 	parseCallback: (results) => {
 		return results[0];
 	},
 });
-export const mkdir = promisify<void,Overloads<typeof fs.mkdir>>(fs.mkdir);
-
-
-export const readdir = promisify<void,NodejsParameters<typeof fs.readdir>>(fs.readdir);
-export const readFile = promisify<void,NodejsParameters<typeof fs.readFile>>(fs.readFile);
-export const readlink = promisify<void,NodejsParameters<typeof fs.readlink>>(fs.readlink);
-export const realpath = promisify<void,NodejsParameters<typeof fs.realpath>>(fs.realpath);
+export const mkdir = promisify<void,NodejsOverloadsParameters<typeof fs.mkdir>>(fs.mkdir);
+export const readdir = promisify<void,NodejsOverloadsParameters<typeof fs.readdir>>(fs.readdir);
+export const readFile = promisify<void,NodejsOverloadsParameters<typeof fs.readFile>>(fs.readFile);
+export const readlink = promisify<void,NodejsOverloadsParameters<typeof fs.readlink>>(fs.readlink);
+export const realpath = promisify<void,NodejsOverloadsParameters<typeof fs.realpath>>(fs.realpath);
 export const rename = promisify<void,NodejsParameters<typeof fs.rename>>(fs.rename);
-export const rmdir = promisify<void,NodejsParameters<typeof fs.rmdir>>(fs.rmdir);
-export const stat = promisify<fs.Stats,NodejsParameters<typeof fs.stat>>(fs.stat);
-export const symlink = promisify<void,NodejsParameters<typeof fs.symlink>>(fs.symlink);
+export const rmdir = promisify<void,NodejsOverloadsParameters<typeof fs.rmdir>>(fs.rmdir);
+export const stat = promisify<fs.Stats,NodejsOverloadsParameters<typeof fs.stat>>(fs.stat);
+export const symlink = promisify<void,NodejsOverloadsParameters<typeof fs.symlink>>(fs.symlink);
 export const unlink = promisify<void,NodejsParameters<typeof fs.unlink>>(fs.unlink);
 export const writeFile = promisify<void,NodejsParameters<typeof fs.writeFile>>(fs.writeFile);  
-export const access = promisify<void,NodejsParameters<typeof fs.access>>(fs.access);
-export const appendFile = promisify<void,NodejsParameters<typeof fs.appendFile>>(fs.appendFile);
+export const access = promisify<void,NodejsOverloadsParameters<typeof fs.access>>(fs.access);
+export const appendFile = promisify<void,NodejsOverloadsParameters<typeof fs.appendFile>>(fs.appendFile);
 export const chmod = promisify<void,NodejsParameters<typeof fs.chmod>>(fs.chmod);
 export const chown = promisify<void,NodejsParameters<typeof fs.chown>>(fs.chown);
 export const close = promisify<void,NodejsParameters<typeof fs.close>>(fs.close);
 export const fchmod = promisify<void,NodejsParameters<typeof fs.fchmod>>(fs.fchmod);
 export const fchown = promisify<void,NodejsParameters<typeof fs.fchown>>(fs.fchown);
-export const rm = promisify<void,NodejsParameters<typeof fs.rm>>(fs.rm);
-export const truncate = promisify<void,NodejsParameters<typeof fs.truncate>>(fs.truncate);
-export const openDir = promisify<void,NodejsParameters<typeof fs.opendir>>(fs.opendir);
-export const open = promisify<void,NodejsParameters<typeof fs.open>>(fs.open);
-export const read = promisify<void,NodejsParameters<typeof fs.read>>(fs.read);
-export const write = promisify<void,NodejsParameters<typeof fs.write>>(fs.write);
-export const mkdtemp = promisify<void,NodejsParameters<typeof fs.mkdtemp>>(fs.mkdtemp);
-export const cp = promisify<void,NodejsParameters<typeof fs.cp>>(fs.cp); 
+export const rm = promisify<void,NodejsOverloadsParameters<typeof fs.rm>>(fs.rm);
+export const truncate = promisify<void,NodejsOverloadsParameters<typeof fs.truncate>>(fs.truncate);
+export const openDir = promisify<void,NodejsOverloadsParameters<typeof fs.opendir>>(fs.opendir);
+export const open = promisify<void,NodejsOverloadsParameters<typeof fs.open>>(fs.open);
+export const read = promisify<void,NodejsOverloadsParameters<typeof fs.read>>(fs.read);
+export const write = promisify<void,NodejsOverloadsParameters<typeof fs.write>>(fs.write);
+export const mkdtemp = promisify<void,NodejsOverloadsParameters<typeof fs.mkdtemp>>(fs.mkdtemp);
+export const cp = promisify<void,NodejsOverloadsParameters<typeof fs.cp>>(fs.cp); 
