304a34651e5eec258bdc06659877f61c703d199a
zhangfisher <wxzhang@126.com>
2023-10-10 18:01:52 +0800

    feat: add `packages/getPackageEntry`

---
+++ src/package/getPackageEntry.ts
@@ -0,0 +1,24 @@
+/**
+ * 读取包的入口文件，即main字段值
+ * 
+ * 
+ */
+import {getPackageJson} from "./getPackageJson"
+import path from "node:path"
+
+export interface GetPackageEntryOptions{
+    entry?:string
+    absolute?:boolean
+}
+
+export function getPackageEntry(options:GetPackageEntryOptions){
+    const { entry=process.cwd(),absolute } = options
+    let entryFile
+    const packageJson = getPackageJson(entry)
+    if(packageJson.main){
+        entryFile =  packageJson.main
+    }else{        
+        entryFile ="./src/index.ts"
+    }
+    return absolute ? path.join(entry || process.cwd(),entryFile) : entryFile
+}
