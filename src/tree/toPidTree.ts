6facd77f7350410523a90180910f4f025a402a38
zhangfisher <wxzhang@126.com>
2023-11-21 15:48:04 +0800

    build: 更新依赖

--- src/tree/toPidTree.ts
+++ src/tree/toPidTree.ts
@@ -17,48 +17,51 @@ import { forEachTreeByDfs } from "./forEachTreeByDfs";
     FromNode extends TreeNodeBase = TreeNode,
     ToNode extends TreeNodeBase = FromNode,
     IdKey extends string = 'id',
-    ChildrenKey extends string = 'children'
+    ChildrenKey extends string = 'children',
+    Path=string
 > extends TreeNodeOptions{
      includeLevel?: boolean
      includePath?: boolean
-     mapper?:({node,level,parent,path,index}:{node:FromNode,level:number,parent?:FromNode | null ,path:string,index:number}) => Omit<ToNode,ChildrenKey | IdKey>
+     mapper?:({node,level,parent,path,index}:{node:FromNode,level:number,parent?:FromNode | null ,path:Path[],index:number}) => Omit<ToNode,ChildrenKey | IdKey>
  }
   
 export type PidTreeNode< 
     Node extends Record<string,any> = {[key: string]: any},
-    IdKey extends string = 'id'
+    IdKey extends string = 'id',
+    Path = string
 > = Node      
     & TreeNodeId<IdKey,Node[IdKey]>  
     & {
         pid:Node[IdKey] | null
         level?:number
-        path?:string
+        path?:Path[]
     } 
 
 export function toPidTree<
     FromNode extends TreeNodeBase = TreeNode,
     ToNode extends TreeNodeBase = FromNode,
     IdKey extends string = 'id',
-    ChildrenKey extends string = 'children'
->(treeObj:FromNode | FromNode[],options?:ToPidTreeOptions<FromNode,ToNode,IdKey,ChildrenKey>):PidTreeNode<Omit<ToNode,ChildrenKey>,IdKey>[]{
+    ChildrenKey extends string = 'children',
+    Path = string
+>(treeObj:FromNode | FromNode[],options?:ToPidTreeOptions<FromNode,ToNode,IdKey,ChildrenKey,Path>):PidTreeNode<Omit<ToNode,ChildrenKey>,IdKey,Path>[]{
     
-    const opts = Object.assign({}, DefaultTreeOptions ,options || {}) as Required<ToPidTreeOptions<FromNode,ToNode,IdKey,ChildrenKey>>     
+    const opts = Object.assign({}, DefaultTreeOptions ,options || {}) as Required<ToPidTreeOptions<FromNode,ToNode,IdKey,ChildrenKey,Path>>     
     const { childrenKey,idKey,includeLevel=true, includePath=false,mapper } = opts
 
-    let nodes:PidTreeNode<Omit<ToNode,ChildrenKey>,IdKey>[] = []
-    forEachTreeByDfs<FromNode>(treeObj,({node,parent,level,path,index})=>{
+    let nodes:PidTreeNode<Omit<ToNode,ChildrenKey>,IdKey,Path>[] = []
+    forEachTreeByDfs<FromNode,Path>(treeObj,({node,parent,level,path,index})=>{
         if(typeof(mapper)=='function'){
             let newNode = Object.assign({
                 [idKey]: node[idKey],
                 pid:null
-            },mapper({node,parent,level,index,path})) as PidTreeNode<ToNode,IdKey>     
+            },mapper({node,parent,level,index,path})) as PidTreeNode<ToNode,IdKey,Path>     
             newNode.pid =parent ? parent?.[idKey]  : null            
             if(includeLevel) newNode.level = level
             if(includePath) newNode.path = path 
             nodes.push(newNode)
             nodes.push()
         }else{
-            let newNode = Object.assign({},omit(node,childrenKey)) as PidTreeNode<ToNode,IdKey>
+            let newNode = Object.assign({},omit(node,childrenKey)) as PidTreeNode<ToNode,IdKey,Path>
             newNode.pid =parent ? parent?.[idKey]  : null
             if(includeLevel) newNode.level = level
             if(includePath) newNode.path = path
