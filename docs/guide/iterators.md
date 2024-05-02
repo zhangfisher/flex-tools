f78493080c31a576d3debd53aeca228ee0a58a8a
wxzhang <wxzhang@126.com>
2024-05-02 15:47:29 +0800

    docs(iterator docs): add flexIterator

--- docs/guide/iterators.md
+++ docs/guide/iterators.md
@@ -18,11 +18,13 @@ canIterate('')  // =true
 
 ## flexIterator
 
-用来创建一个迭代器，`FlexIterator`支持构建参数：
+根据可迭代对象可迭代创建器，`FlexIterator`支持构建参数：
 
 ```ts
 
-class FlexIterator<Value=any,Result=Value,Parent=Value>;
+class FlexIterator<Value=any,Result=Value,Parent=Value>{
+    constructor(values:IteIterable<Value | Iterable<any>>options?:FlexIteratorOptions<Value>)    
+}
 
 interface FlexIteratorOptions<T> {
     filter?:(item:Value | Parent)=>Value | Iterable<any> | {value:Value | Iterable<any>,parent:Parent}
@@ -31,68 +33,104 @@ interface FlexIteratorOptions<T> {
     recursion?:boolean
 }
 ```
-
-- `类型`
-
-    - `Value`：迭代的值的类型。
-    - `Result`：迭代的结果的类，即transform返回结果值。
-    - `Parent`：父级迭代的值的类型，当启用递归迭代时，用来传递父级迭代的值。
-
-
-- `filter`：
-
-用来将迭代的值进行预处理和过滤，该返回可以返回:
-
-    - Value:   一个新的值
-    - 可迭代对象：如果返回一个可迭代对象并且`recursion=true`，将会对该对象进行迭代。
-    - {value:Value,parent:Parent}：返回一个新的值和父级迭代对象。
-
-
-
-- `transform`：
-
-用来对迭代的值进行转换。
-
-
+ 
 
 **示例：**
+
+- **迭代输入对象，通过`transform`转换值**
 
 ```ts
 
 import { FlexIterator } from 'flex-tools/iterators/flexIterator';
 
-const source = new FlexIterator([1,2,3,4,5])
-for(let value of source){
- console.log(value)
-}
-// Output: 1,2,3,4,5
 
-const source = new FlexIterator([1,2,3,4,5],{
-    transform:(value)=>`S-${value}`         // 对迭代的值进行转换
-})
-
-for(let value of source){
- console.log(value)
+const i1 = new FlexIterator([1,2,3,4,5],{transform:(value)=>`S-${value}`})
+for(let value of i1){
+    console.log(value,",")
 }
 // Output: S-1,S-2,S-3,S-4,S-5
+```
 
-const source = new FlexIterator([1,2,[3,4],[5,6,[7,8,[9,10]]]],{
- transform:(value)=>`S-${value}`
- // 当transform返回一个迭代对象时，递归遍历所有可迭代对象
- recursion:true             
-})
-for(let value of source){
- console.log(String(value))
+- **默认`recursion:false`，不递归遍历**
+
+```ts 
+const i2 = new FlexIterator([1,[2,3],[4,5]],{transform:(value)=>`S-${value}`})
+for(let value of i2){
+    console.log(value)
 }
-//  Output: S-1,S-2,S-3,S-4,S-5,S-6,S-7,S-8,S-9,S-10
-const source = new FlexIterator([1,2,[3,4],[5,6,[7,8,[9,10]]]],{
- transform:(value)=>`S-${value}`
- recursion:false         // 不对迭代对象进行递归迭代
-})
-for(let value of source){
- console.log(String(value))
+
+// Output: S-1,S-2,3,S-4,5
+```
+- **指定`recursion:true`时，递归遍历**
+
+```ts
+const i3 = new FlexIterator([1,[2,3],[4,5]],{transform:(value)=>`S-${value}`,recursion:true})
+for(let value of i3){
+    console.log(value)
 }
-//  Output: S-1,S-2,S-[3,4],S-[5,6,[7,8,[9,10]]]
+// Output: S-1,S-2,S-3,S-4,S-5
+```
+
+- **通过`pick`函数从迭代项中提取内容进行迭代**
+
+```ts
+const i4 = new FlexIterator(["A","AA","AAA","AAAA","AAAAA"],{
+    pick:(value)=>String(value.length),
+    transform:(value)=>`S-${value}`
+})
+for(let value of i4){
+    console.log(value)
+}
+// Output: S-1,S-2,S-3,S-4,S-5
+```
+
+- **当进行递归遍历时，`transform`函数可以接收第二个参数`parent`，表示当前迭代项的父级迭代项。**
+
+```ts
+const i5 = new FlexIterator([1,[2,3],[4,5]],{
+    pick:(value)=>value,
+    transform:(value,parent)=>`S-${value} (parent=${parent})`,
+    recursion:true
+})
+for(let value of i5){
+    console.log(value)
+}
+// Output: 
+// S-1 (parent=undefined)
+// S-2 (parent=2,3)
+// S-3 (parent=2,3)
+// S-4 (parent=4,5)
+// S-5 (parent=4,5) 
 
 ```
+
+- **当进行递归遍历时，在`pick`中可以对当前迭代项的父级迭代项`parent`包装**
+
+```ts
+const i6 = new FlexIterator([1,[2,3],[4,5]],{
+    pick:(value)=>{
+        if(Array.isArray(value)){
+            return {
+                value,
+                parent:`P_${value.join("_")}`
+            }
+        }else{  
+            return value    
+        }
+        
+    },
+    transform:(value,parent)=>`S-${value} (parent=${parent})`,
+    recursion:true
+})
+for(let value of i6){
+    console.log(value)
+}
+// Output: 
+// S-1 (parent=undefined)
+// S-2 (parent=P_2_3)
+// S-3 (parent=P_2_3)
+// S-4 (parent=P_4_5)
+// S-5 (parent=P_4_5)
+```
+
 
--- src/iterators/flexIterator.ts
+++ src/iterators/flexIterator.ts
@@ -1,6 +1,7 @@
+import { isPlainObject } from "../typecheck"
 import { canIterator } from "./canIterator"
 export type FlexIteratorOptions<Value=any,Result=Value,Parent=any> = {
-    filter?:(item:Value | Parent)=>Value | Iterable<any>
+    pick?:(item:Value | Parent)=>Value | Iterable<any>
     transform:(value:Value,parent?:Parent)=>Result
     // 当true时如果transform也返回一个迭代对象时，递归遍历所有可迭代对象
     recursion?:boolean
@@ -38,7 +39,7 @@ export type FlexIteratorOptions<Value=any,Result=Value,Parent=any> = {
  * @param nodes 
  * @param options 
  */
-export class FlexIterator<Value=any,Result=Value,Parent=Value> {
+export class FlexIterator<Value=any,Result=Value,Parent=any> {
     options: FlexIteratorOptions<Value,Result,Parent>
     constructor(private nodes: Iterable<Value | Iterable<any>>,options?:FlexIteratorOptions<Value,Result,Parent>) {
         this.options = Object.assign({
@@ -47,7 +48,7 @@ export class FlexIterator<Value=any,Result=Value,Parent=Value> {
     }  
     [Symbol.iterator](): Iterator<Result, any, undefined> {
         let value : any
-        const {filter: pick,transform,recursion} = this.options
+        const {pick: pick,transform,recursion} = this.options
         const transformValue = typeof(transform)=='function' ? (value:any,parent?:Parent)=>transform(value,parent) : (value:any,parent?:Parent)=>value
         const pickItemValue = typeof(pick) == 'function' ? (value:Value | Parent)=>pick(value) : (value:any)=>value
         let sources:Iterator<any>[] = [this.nodes[Symbol.iterator]()]  
@@ -65,12 +66,17 @@ export class FlexIterator<Value=any,Result=Value,Parent=Value> {
                         return {done: true, value: undefined} 
                     }                    
                 }else{
-                    const itemValue = pickItemValue(value.value)
+                    const itemValue = pickItemValue(value.value)                     
                     if(recursion && canIterator(itemValue)){
-                        sources.push(itemValue[Symbol.iterator]())
-                        curSource = sources[sources.length-1]    
-                        parentValue = value.value
+                            sources.push(itemValue[Symbol.iterator]())
+                            curSource = sources[sources.length-1]    
+                            parentValue = value.value
                         return this.next() 
+                    }else if(isPlainObject(itemValue) && 'value' in itemValue && 'parent' in itemValue){  
+                        sources.push(itemValue.value[Symbol.iterator]())
+                        curSource = sources[sources.length-1]    
+                        parentValue = itemValue.parent
+                        return this.next()
                     }else{                                           
                         return {done:false,value:transformValue(itemValue,parentValue as any)}
                     }
@@ -84,3 +90,78 @@ export class FlexIterator<Value=any,Result=Value,Parent=Value> {
     }
     
 }
+
+
+
+const i1 = new FlexIterator([1,2,3,4,5],{transform:(value)=>`S-${value}`})
+for(let value of i1){
+    console.log(value,",")
+}
+// Output: S-1,S-2,S-3,S-4,S-5
+console.log("---")
+
+const i2 = new FlexIterator([1,[2,3],[4,5]],{transform:(value)=>`S-${value}`})
+for(let value of i2){
+    console.log(value)
+}
+
+// Output: S-1,S-2,3,S-4,5
+console.log("---")
+const i3 = new FlexIterator([1,[2,3],[4,5]],{transform:(value)=>`S-${value}`,recursion:true})
+for(let value of i3){
+    console.log(value)
+}
+// Output: S-1,S-2,S-3,S-4,S-5
+console.log("---")
+const i4 = new FlexIterator(["A","AA","AAA","AAAA","AAAAA"],{
+    pick:(value)=>String(value.length),
+    transform:(value)=>`S-${value}`
+})
+for(let value of i4){
+    console.log(value)
+}
+// Output: S-1,S-2,S-3,S-4,S-5
+console.log("---")
+const i5 = new FlexIterator([1,[2,3],[4,5]],{
+    pick:(value)=>value,
+    transform:(value,parent)=>`S-${value} (parent=${parent})`,
+    recursion:true
+})
+for(let value of i5){
+    console.log(value)
+}
+// Output: 
+// S-1 (parent=undefined)
+// S-2 (parent=2,3)
+// S-3 (parent=2,3)
+// S-4 (parent=4,5)
+// S-5 (parent=4,5)
+console.log("---")
+
+// Output: S-1,S-2,S-3,S-4,S-5
+console.log("---")
+const i6 = new FlexIterator([1,[2,3],[4,5]],{
+    pick:(value)=>{
+        if(Array.isArray(value)){
+            return {
+                value,
+                parent:`P_${value.join("_")}`
+            }
+        }else{  
+            return value    
+        }
+        
+    },
+    transform:(value,parent)=>`S-${value} (parent=${parent})`,
+    recursion:true
+})
+for(let value of i6){
+    console.log(value)
+}
+// Output: 
+// S-1 (parent=undefined)
+// S-2 (parent=2,3)
+// S-3 (parent=2,3)
+// S-4 (parent=4,5)
+// S-5 (parent=4,5)
+console.log("---")
