d242bd212a31369e7a89feea3d7fea606da67124
zhangfisher <wxzhang@126.com>
2024-07-11 16:10:30 +0800

    fix(types): 修复`DeepPartial`和`DeepRequired`的错误

--- src/types/deepPartial.ts
+++ src/types/deepPartial.ts
@@ -1,5 +1,5 @@
 import { Dict } from "./dict";
 
 export type DeepPartial<T extends Dict> = {
-    [P in keyof T]?: T[P] extends Dict ? DeepPartial<T[P]> : T[P];
+    [P in keyof T]?: T[P] extends Dict ? DeepPartial<T[P]> : Partial<T[P]>;
 }
--- src/types/deepRequired.ts
+++ src/types/deepRequired.ts
@@ -1,5 +1,5 @@
 import { Dict } from "./dict";
 
 export type DeepRequired<T extends Dict> = {
-    [P in keyof T]-?: T[P] extends Dict ? DeepRequired<T[P]> : T[P];
+    [P in keyof T]-?: T[P] extends Dict ? DeepRequired<T[P]> : Required<T[P]>;
 }
