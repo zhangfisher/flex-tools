24ea0c4243c721e7667e6561d4cde65030fae50c
zhangfisher <wxzhang@126.com>
2024-11-28 15:15:57 +0800

    update

--- src/events/liteEvent.ts
+++ src/events/liteEvent.ts
@@ -2,10 +2,16 @@
 * 一个简单的事件触发器 
 */
  
+export interface FlexLiteEvents{
+
+}
 
 export interface LiteEventSubscribeOptions{
     objectify?: boolean                 //  当调用时返回一个对象用来退订
     count?:number                       // 触发几次
+}
+export interface LiteEventOptions{
+    ignoreError?:boolean                // 执行侦听器出错时是否忽略错误
 }
 
 export interface LiteEventListener<Message=any>{
@@ -13,7 +19,7 @@ export interface LiteEventListener<Message=any>{
 }
 
 
-export type ForEachLiteEventListenerCallback<Message,Events> = ({event,listenerId,listener,count,eventListeners}:{event:Events,listenerId:number,listener:LiteEventListener<Message>,count:number,eventListeners:LiteEventListenerRegistry<Message>})=>boolean | void
+export type ForEachLiteEventListenerCallback<Events,Message> = ({event,listenerId,listener,count,eventListeners}:{event:Events,listenerId:number,listener:LiteEventListener<Message>,count:number,eventListeners:LiteEventListenerRegistry<Message>})=>boolean | void
 
 
 export interface LiteEventSubscriber{
@@ -22,15 +28,25 @@ export interface LiteEventSubscriber{
 
 
 export type LiteEventListenerRegistry<M> = Map<number,[LiteEventListener<M>,number]>
-export type LiteListenerRegistry<M,E> = Map<E,LiteEventListenerRegistry<M>>
+export type LiteListenerRegistry<E,M> = Map<E,LiteEventListenerRegistry<M>>
 
 
-export class LiteEvent<Message=any,Events extends string = string>{
+export class LiteEvent<
+    Events extends FlexLiteEvents = FlexLiteEvents,
+    EventNames extends keyof Events = keyof Events,
+    Message extends Events[EventNames] = Events[EventNames]
+>{
      // {"<事件名称>":{<listenerId>:[Callback,<侦听次数>]}}
-     private _listeners:LiteListenerRegistry<Message,Events>= new Map()
+     private _listeners:LiteListenerRegistry<EventNames,Message>= new Map()
      // 保留最后一次触发的消息,key=事件名称,value=消息
      private _lastMessage:Record<string,any> = {}        
      static listenerSeqId:number = 0
+     options:Required<LiteEventOptions>
+     constructor(options?:LiteEventOptions){
+        this.options = Object.assign({
+            ignoreError:false
+        },options) as Required<LiteEventOptions>
+     }
      get listeners(){return this._listeners}
      /**
       * 订阅事件并返回一个事件订阅ID
@@ -40,36 +56,43 @@ export class LiteEvent<Message=any,Events extends string = string>{
       * @param options 
       * @returns 
       */
-     on(event:Events,callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions):LiteEventSubscriber | number{
-         const { objectify = false,count=-1 } = Object.assign({},options) as Required<LiteEventSubscribeOptions>        
-         if(!this._listeners.has(event)){
+     on(event:EventNames,callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions):LiteEventSubscriber | number
+     on(event:'*',callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions):LiteEventSubscriber | number
+     on():LiteEventSubscriber | number{
+        const event = arguments[0] as EventNames
+        const callback = arguments[1] as LiteEventListener<Message>
+        const options = arguments[2] as LiteEventSubscribeOptions
+        const { objectify=false,count=-1 } = Object.assign({},options) as Required<LiteEventSubscribeOptions>        
+        if(!this._listeners.has(event)){
              this._listeners.set(event,new Map())        
-         }
-         const listenerId =  ++LiteEvent.listenerSeqId            
-         const eventListeners = this._listeners.get(event) as LiteEventListenerRegistry<Message>
-         eventListeners?.set(listenerId,[callback,count])        
-         // 如果启用了retain,则应该马上触发最后保存的事件
-         this.emitRetainEvent(event,listenerId,eventListeners)      
-         if(objectify){
-             return {
-                 off:()=>{
-                     eventListeners?.delete(listenerId)
-                     if(eventListeners?.size==0){
-                         this._listeners.delete(event)
-                     }
-                 }
-             }
-         }else{
-             return listenerId
-         }
+        }
+        const listenerId =  ++LiteEvent.listenerSeqId            
+        const eventListeners = this._listeners.get(event) as LiteEventListenerRegistry<Message>
+        eventListeners?.set(listenerId,[callback,count])        
+        this.emitRetainEvent(event,listenerId,eventListeners)      
+        if(objectify){
+            return {
+                off:()=>{
+                    eventListeners?.delete(listenerId)
+                    if(eventListeners?.size==0){
+                        this._listeners.delete(event)
+                    }
+                }
+            }
+        }else{
+            return listenerId
+        }
      } 
+     onAny(callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions):LiteEventSubscriber | number{
+        return this.on('*',callback,options)
+     }
      /**
       * 如果事件已经有最近触发时保留的数据，则立即触发事件将最近的数据传递给侦听器
       * @param event 
       */
-     private emitRetainEvent(event:Events,listenerId:number,eventListeners:LiteEventListenerRegistry<Message>){
+     private emitRetainEvent(event:EventNames,listenerId:number,eventListeners:LiteEventListenerRegistry<Message>){
         if(event in this._lastMessage){
-            this.executeListener(listenerId,eventListeners,this._lastMessage[event])   
+            this.executeListener(listenerId,eventListeners,this._lastMessage[event as string])   
         }        
      }
      /**
@@ -79,7 +102,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
       * @param options 
       * @returns 
       */
-     once(event:Events,callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions){
+     once(event:EventNames,callback:LiteEventListener<Message>,options?:LiteEventSubscribeOptions){
          return this.on(event,callback,Object.assign({},options,{count:1}))        
      }  
  
@@ -88,16 +111,15 @@ export class LiteEvent<Message=any,Events extends string = string>{
       *   {"<事件名称>":{<listenerId>:[Callback,<侦听次数>]}}
       * @param callback  ={}
       */
-     private forEachListeners(callback:({event,listenerId,listener,count,eventListeners}:{event:Events,listenerId:number,listener:LiteEventListener<Message>,count:number,eventListeners:LiteEventListenerRegistry<Message>})=>boolean | void){
+     private forEachListeners(callback:({event,listenerId,listener,count,eventListeners}:{event:EventNames,listenerId:number,listener:LiteEventListener<Message>,count:number,eventListeners:LiteEventListenerRegistry<Message>})=>boolean | void){
          // {"<事件名称>":{<listenerId>:[Callback,<侦听次数>]}}
          let isAbort = false
          for(let [event,eventListeners] of this._listeners.entries()){
              if(isAbort) break
              for(let [listenerId,[listener,count]] of eventListeners.entries()){
                  if(isAbort) break
-                 let r = callback({event,listenerId,listener,count,eventListeners})
-                 // 显式返回false时中止遍历
-                 isAbort = r === false
+                 let r = callback({event,listenerId,listener,count,eventListeners})                
+                 isAbort = r === false // 显式返回false时中止遍历
              }
          }         
      }
@@ -110,10 +132,10 @@ export class LiteEvent<Message=any,Events extends string = string>{
       * @param callback 
       * @returns 
       */
-     private forEachEventListeners(event:Events,callback:ForEachLiteEventListenerCallback<Message,Events>){
+     private forEachEventListeners(event:EventNames,callback:ForEachLiteEventListenerCallback<EventNames,Message>){
          // {"<事件名称>":{<listenerId>:[Callback,<侦听次数>]}}        
          let isAbort = false        
-         let matchedListeners = [[event,this._listeners.get(event)]] as [Events,LiteEventListenerRegistry<Message> | undefined][]       
+         let matchedListeners = [[event,this._listeners.get(event)]] as [EventNames,LiteEventListenerRegistry<Message> | undefined][]       
          for(let [eventName,eventListeners] of matchedListeners){
              if(!eventListeners) continue
              for(let [listenerId,[listener,count]] of eventListeners){
@@ -142,7 +164,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
       */ 
       off(listener:LiteEventListener<Message>):void;
       off(listenerId:number):void;
-      off(event:Events,listener:LiteEventListener<Message>):void;
+      off(event:EventNames,listener:LiteEventListener<Message>):void;
       off(){
          // {"<事件名称>":{<listenerId>:[Callback,<侦听次数>]}}
         if(arguments.length==1){
@@ -165,7 +187,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
              }
         }else if(arguments.length==2){
              if(typeof(arguments[0])=='string' && typeof(arguments[1])=='function'){// off(event,callback)
-                 this.forEachEventListeners(arguments[0] as Events,({event,listenerId,listener,eventListeners})=>{
+                 this.forEachEventListeners(arguments[0] as EventNames,({event,listenerId,listener,eventListeners})=>{
                      if(event == arguments[0] && listener ==  arguments[1] ){
                          eventListeners.delete(listenerId) 
                          if(this._listeners.get(event)?.size==0) this._listeners.delete(event)
@@ -179,7 +201,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
       * 等待某个事件触发后返回
       * @param event  事件名称
       */
-     waitFor(event:Events,timeout:number=0){        
+     waitFor(event:EventNames,timeout:number=0){        
          return new Promise<Message>((resolve,reject)=>{
              let tmId:any,isTimeout:boolean=false 
              let listenerId:number
@@ -197,7 +219,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
             }) as number
         })
     }
-     offAll(event?:Events){
+     offAll(event?:EventNames){
          if(event){
              this._listeners.delete(event)
          }else{
@@ -219,6 +241,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
          try{
             return listener[0](message!) 
          }catch(e){
+            if(!this.options.ignoreError) throw e
          }finally{
              if(listener[1]>-1){
                  listener[1] = listener[1]-1
@@ -228,7 +251,7 @@ export class LiteEvent<Message=any,Events extends string = string>{
              }            
          }  
      }   
-     private executeListeners(event:Events,message?:Message,callback?:(listenerId?:number)=>void){
+     private executeListeners(event:EventNames,message?:Message,callback?:(listenerId?:number)=>void){
          let results:any[] = []
          this.forEachEventListeners(event,({event:eventName,listenerId,eventListeners})=>{
              results.push(this.executeListener(listenerId,eventListeners,message))
@@ -244,10 +267,24 @@ export class LiteEvent<Message=any,Events extends string = string>{
       * @param event 
       * @param message 
       */
-     emit(event:Events,message?:Message,retain?:boolean){
+     emit(event:EventNames,message?:Message,retain?:boolean){
          if(retain){
-             this._lastMessage[event] = message
+             this._lastMessage[event as any] = message
          }
          return this.executeListeners(event,message)
      }
  } 
+
+
+const ev = new LiteEvent<{
+    a:number,
+    b:boolean,
+    c:{count:number}
+}>()
+
+
+ev.on("a",(message)=>{
+
+})
+
+
