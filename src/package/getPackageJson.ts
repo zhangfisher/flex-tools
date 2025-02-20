b55e98b552ac9dadc9e7cf6e6b17f49d8f5cebcb
wxzhang <wxzhang@126.com>
2025-02-20 11:57:46 +0800

    feat: getPackageJson增加一个findUp参数

--- src/package/getPackageJson.ts
+++ src/package/getPackageJson.ts
@@ -2,9 +2,14 @@ import type { PackageJson } from "type-fest";
 import { getPackageRootPath } from "./getPackageRootPath";
 import path from "node:path"
 
-
-export function getPackageJson(entry?:string):PackageJson | undefined{
-    const packageRoot = getPackageRootPath(entry) as string
+/**
+ * 
+ * @param entry 
+ * @param findUp   是否向上查找package.json，默认为true
+ * @returns 
+ */
+export function getPackageJson(entry?:string,findUp?:boolean):PackageJson | undefined{
+    const packageRoot =findUp ? getPackageRootPath(entry) as string : entry || process.cwd()
     try{
         return require(path.join(packageRoot,"package.json")) 
     }catch{
