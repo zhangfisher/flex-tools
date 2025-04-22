cf22fcf6ecf9ffb4e605497862950c4e39a95ebd
wxzhang <wxzhang@126.com>
2025-04-22 17:41:07 +0800

    fix: 修复remove函数的返回值类型

--- src/array/index.ts
+++ src/array/index.ts
@@ -4,14 +4,14 @@ import { remove } from "./remove"
 
 declare global {
     interface Array<T> {
-        remove<T>(...values: (T | ({ returnRemoved?: boolean }))[]): T | number;         
+        remove<T>(...values: T[]): T | number;         
         get<T=any>(index:number,defaultValue?:T | undefined):T | undefined; 
     }
 }
 
 
-Array.prototype.remove=function<T>(this:Array<any>,...values: (T | ({ returnRemoved?: boolean }))[]){   
-    return remove<T>(this,...values) as T | number
+Array.prototype.remove=function<T>(this:Array<any>,...values: T[]){   
+    return remove<T>(this,...values) 
 }
 Array.prototype.get=function<T=any>(this:Array<T>,index:number,defaultValue?:T | undefined):T | undefined{   
     return getItem<T>(this,index,defaultValue)
--- src/array/remove.ts
+++ src/array/remove.ts
@@ -3,9 +3,7 @@
  * 
  * @param arr - 要操作的数组
  * @param values - 要移除的值列表
- * @param options - 配置选项
- * @param options.returnRemoved - 是否返回被移除的元素（默认false，返回移除的数量）
- * @returns 如果returnRemoved为true，返回被移除的元素数组；否则返回移除的元素数量
+ * @returns 返回移除的元素数量
  * 
  * @example
  * ```typescript
@@ -15,31 +13,18 @@
  * remove(arr, 2, 3);  // 返回: 3
  * console.log(arr);   // 输出: [1, 4, 5]
  * 
- * // 移除值为4和5的元素，返回被移除的元素
- * remove(arr, 4, 5, { returnRemoved: true });  // 返回: [4, 5]
- * console.log(arr);   // 输出: [1]
  * ```
  */
-export function remove<T>(
-    arr: T[],
-    ...args: (T | { returnRemoved?: boolean })[]
-): number | T[] {
-    // 提取选项参数
-    const options = args.length > 0 && typeof args[args.length - 1] === 'object'
-        ? args.pop() as { returnRemoved?: boolean }
-        : { returnRemoved: false };
-    
+export function remove<T>(arr: T[],...args: T[]): number {
     const values = args as T[];
     const valueSet = new Set(values);
     const removed: T[] = [];
-
     // 从后向前遍历以避免splice导致的索引问题
     for (let i = arr.length - 1; i >= 0; i--) {
         if (valueSet.has(arr[i])) {
             removed.unshift(arr.splice(i, 1)[0]);
         }
     }
-
-    return options.returnRemoved ? removed : removed.length;
+    return  removed.length;
 }
  
