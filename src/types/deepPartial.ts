3ae0e35c5679589b92c74715209ac7214341bc34
wxzhang <wxzhang@126.com>
2025-05-26 14:43:34 +0800

    fix: update DeepOptional and DeepRequired

--- src/types/deepPartial.ts
+++ src/types/deepPartial.ts
@@ -1,4 +1,19 @@
-export type DeepPartial<T> = {
-    [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
-}
+export type DeepPartial<T> =  T extends (...args: any[]) => any
+? T
+: T extends Map<infer K, infer V>
+  ? Map<DeepPartial<K>, DeepPartial<V>>
+: T extends Set<infer U>
+  ? Set<DeepPartial<U>>
+: T extends ReadonlyArray<infer U>
+  ? T extends readonly [infer First, ...infer Rest]
+    ? readonly [DeepPartial<First>?, ...DeepPartial<Rest>]
+    : ReadonlyArray<DeepPartial<U>>
+: T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    } & (T extends { [key: string]: infer V } 
+      ? { [key: string]: DeepPartial<V> } 
+      : {})
+: T;
 export type DeepOptional<T> = DeepPartial<T> 
+ 
--- src/types/deepRequired.ts
+++ src/types/deepRequired.ts
@@ -28,6 +28,36 @@
  * type ArrayExample = Array<{ id?: number }>;
  * type RequiredArray = DeepRequired<ArrayExample>; // 结果为Array<{ id: number }>
  */
-export type DeepRequired<T> = {
-    [P in keyof T]-?: T[P] extends object ? DeepRequired<T[P]> : T[P];
-} 
+export type DeepRequired<T> =  T extends (...args: any[]) => any
+    ? T  // 函数类型保持不变
+    : T extends Map<infer K, infer V>
+    ? Map<DeepRequired<K>, DeepRequired<V>>  // 处理Map
+    : T extends Set<infer U>
+    ? Set<DeepRequired<U>>  // 处理Set
+    : T extends ReadonlyArray<infer U>
+    ? T extends readonly [infer First, ...infer Rest]
+        ? readonly [DeepRequired<First>, ...DeepRequired<Rest>]  // 处理元组
+        : ReadonlyArray<DeepRequired<U>>  // 处理数组
+    : T extends object
+    ? {
+        [P in keyof T]-?: DeepRequired<T[P]>;
+        }
+    : NonNullable<T>;
+
+
+
+    // type Example = {
+    //     a?: string;
+    //     b?: {
+    //       c?: number;
+    //       d?: {
+    //         e?: boolean;
+    //       };
+    //     };
+    //     f?: string[];
+    //     g?: [number?, boolean?];
+    //     h?: Map<string, { value?: number }>;
+    //     i?: Set<{ id?: string }>;
+    //     j?: () => void;
+    //   };
+      
